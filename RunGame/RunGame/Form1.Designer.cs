6e7f82d54403d18b80bf1261d939861193a04448
osama-akp <119467652+osama-akp@users.noreply.github.com>
2023-04-28 17:18:54 +0500

    Add files via upload

---
+++ RunGame/RunGame/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace RunGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblhighScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::RunGame.Properties.Resources.platform_tilesblue;
+            this.pictureBox1.Location = new System.Drawing.Point(-8, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(894, 62);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::RunGame.Properties.Resources.platform_tilesblue;
+            this.pictureBox2.Location = new System.Drawing.Point(-8, 397);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(894, 62);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::RunGame.Properties.Resources.run_up0;
+            this.player.Location = new System.Drawing.Point(118, 50);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(59, 74);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 3;
+            this.player.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::RunGame.Properties.Resources.box;
+            this.pictureBox4.Location = new System.Drawing.Point(385, 56);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(46, 77);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "obstacle";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::RunGame.Properties.Resources.box;
+            this.pictureBox3.Location = new System.Drawing.Point(620, 322);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(46, 77);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacle";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.ForeColor = System.Drawing.Color.White;
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(121, 38);
+            this.lblScore.TabIndex = 7;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // lblhighScore
+            // 
+            this.lblhighScore.AutoSize = true;
+            this.lblhighScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblhighScore.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblhighScore.ForeColor = System.Drawing.Color.White;
+            this.lblhighScore.Location = new System.Drawing.Point(12, 403);
+            this.lblhighScore.Name = "lblhighScore";
+            this.lblhighScore.Size = new System.Drawing.Size(200, 38);
+            this.lblhighScore.TabIndex = 8;
+            this.lblhighScore.Text = " High Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RunGame.Properties.Resources.background_still;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblhighScore);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Gravity Run Game MOOICT";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblhighScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ RunGame/RunGame/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunGame
+{
+    public partial class Form1 : Form
+    {
+        // created by MOO ICT 2022
+        // for educational purpose only
+        // global variables
+        int gravity;
+        int gravityValue = 8;
+        int obstacleSpeed = 10;
+        int score = 0;
+        int highScore = 0;
+        bool gameOver = false;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            lblScore.Text = "Score: " + score;
+            lblhighScore.Text = "High Score: " + highScore;
+            player.Top += gravity;
+            // when the player land on the platforms. 
+            if (player.Top > 331)
+            {
+                gravity = 0;
+                player.Top = 331;
+                player.Image = Properties.Resources.run_down0;
+            }
+            else if (player.Top < 50)
+            {
+                gravity = 0;
+                player.Top = 50;
+                player.Image = Properties.Resources.run_up0;
+            }
+            // move the obstacles
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+                    if (x.Left < -100)
+                    {
+                        x.Left = random.Next(1200, 3000);
+                        score += 1;
+                    }
+
+                    if (x.Bounds.IntersectsWith(player.Bounds))
+                    {
+                        gameTimer.Stop();
+                        lblScore.Text += " Game Over!! Press Enter to Restart.";
+                        gameOver = true;
+                        // set the high score 
+                        if (score > highScore)
+                        {
+                            highScore = score;
+                        }
+                    }
+                }
+            }
+            // increase the speed of player and obstacles
+            if (score > 10)
+            {
+                obstacleSpeed = 20;
+                gravityValue = 12;
+            }
+        }
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                if (player.Top == 331)
+                {
+                    player.Top -= 10;
+                    gravity = -gravityValue;
+                }
+                else if (player.Top == 50)
+                {
+                    player.Top += 10;
+                    gravity = gravityValue;
+                }
+            }
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
+        }
+        private void RestartGame()
+        {
+            lblScore.Parent = pictureBox1;
+            lblhighScore.Parent = pictureBox2;
+            lblhighScore.Top = 0;
+            player.Location = new Point(123, 191);
+            player.Image = Properties.Resources.run_down0;
+            score = 0;
+            gravityValue = 8;
+            gravity = gravityValue;
+            obstacleSpeed = 10;
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left = random.Next(1200, 3000);
+                }
+            }
+            gameTimer.Start();
+        }
+    }
+}
